a6fcd5d245a33ce4ac218b523620477bf4abed8b
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-10-03 23:24:42 +0200

    Only admin can change content- fixed admin.py

--- qfb_main/admin.py
+++ qfb_main/admin.py
@@ -2,16 +2,16 @@ from django.contrib import admin
 from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
-
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-
     list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ['title', 'content']
     list_filter = ('status', 'created_on')
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
