6a5c2e546d8f2b04b45cf70b6957a331db2644af
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-10-08 17:38:44 +0200

    Removing truncation from template

--- quickfire_bulletin/urls.py
+++ quickfire_bulletin/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),  # Add this line
     path("", views.NewsArticleList.as_view(), name="home"),
 ]
