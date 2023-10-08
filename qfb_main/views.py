cbbb938c56100aadbd8aadd8809ad3432894582e
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-10-08 19:28:26 +0200

    Adding rendering to view

--- qfb_main/views.py
+++ qfb_main/views.py
@@ -1,13 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
 import os
 import requests
 import json
 import logging
 from datetime import datetime
 from django.utils import timezone
-from django.utils.text import slugify 
-import uuid 
+from django.utils.text import slugify
+import uuid
 from qfb_main.models import NewsArticle
 
+# Function to fetch news
 def fetch_news():
     api_key = os.getenv('NEWS_API_KEY')
     url = f"https://newsdata.io/api/1/news?apikey={api_key}&country=us&language=en"
@@ -17,30 +20,27 @@ def fetch_news():
         try:
             data = json.loads(response.text)
             articles = data['results']
-
             for article in articles:
                 try:
                     pub_date_str = article.get('pubDate', None)
                     if pub_date_str:
                         pub_date = datetime.strptime(pub_date_str, '%Y-%m-%d %H:%M:%S')  
                         pub_date = timezone.make_aware(pub_date)
-
-              
                     slug = slugify(article['title']) + '-' + str(uuid.uuid4())[:8]
-
                     news_article, created = NewsArticle.objects.update_or_create(
                         article_id=article['article_id'],
                         defaults={
                             'title': article['title'],
-                            'slug': slug,  
+                            'slug': slug,
                             'content': article.get('content', ''),
-                            'author_id': 1,  
+                            'author_id': 1,
                             'source_id': article['source_id'],
                             'source_priority': article['source_priority'],
-                            'category': ','.join(article['category']), 
+                            'category': ','.join(article['category']),
                             'language': article['language'],
                             'pubDate': pub_date if pub_date_str else None,
-                            'image_url': article.get('image_url', ''), 
+                            'image_url': article.get('image_url', ''),
+                            'status': 1
                         }
                     )
                     logging.debug(f"Saved article with ID: {article['article_id']}")
@@ -52,3 +52,8 @@ def fetch_news():
             logging.error(f"An error occurred while processing the API response: {e}")
     else:
         logging.error(f"API call failed with status code {response.status_code}: {response.text}")
+
+# Function to render news articles
+def news_list(request):
+    articles = NewsArticle.objects.filter(status=1)
+    return render(request, 'index.html', {'NewsArticle_list': articles})
