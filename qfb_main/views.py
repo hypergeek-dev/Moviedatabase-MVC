6dfb218ddc19be6377a34d7f66dd98b2b6885fbb
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-10-09 17:41:41 +0200

    Moving button to index

--- qfb_main/views.py
+++ qfb_main/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 import os
 import requests
