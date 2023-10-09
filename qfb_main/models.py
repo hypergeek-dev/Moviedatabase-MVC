2cd31f6e43ed3b49e904dde42fbb7186f16b1f54
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-10-09 16:16:04 +0200

    Further changes to classnames to avoid mismatch

--- qfb_main/models.py
+++ qfb_main/models.py
@@ -32,7 +32,7 @@ class Comment(models.Model):
     NewsArticle = models.ForeignKey(NewsArticle, on_delete=models.CASCADE, related_name="comments")
     name = models.CharField(max_length=80)
     email = models.EmailField()
-    comment__content = models.TextField()
+    comment_content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
 
