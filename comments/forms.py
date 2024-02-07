a6e14dbc3ca7300de0406f4a546ceb44b449d8b4
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-02-07 20:38:09 +0000

    Removing redundant fields from forms

--- comments/forms.py
+++ comments/forms.py
@@ -1,13 +1,9 @@
 from django import forms
 from .models import Comment
 
-
 class CommentForm(forms.ModelForm):
-    name = forms.CharField(max_length=100, required=True)
-    email = forms.EmailField(max_length=100, required=True)
     comment_content = forms.CharField(widget=forms.Textarea, required=True)
 
     class Meta:
         model = Comment
-        fields = ('name', 'email', 'comment_content')
-        
+        fields = ('comment_content',)
