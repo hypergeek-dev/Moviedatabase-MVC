bab9880cb6827c9516fedd7accc4559b4a6c4ab0
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-02-07 23:27:48 +0100

    formatting view in main app

--- qfb_main/views.py
+++ qfb_main/views.py
@@ -27,6 +27,8 @@ nlp = spacy.load("en_core_web_sm")
 logger = logging.getLogger(__name__)
 
 # Function to fetch news
+
+
 def fetch_news(request=None):
     response = make_api_call()
     if response.status_code == 200:
@@ -37,19 +39,21 @@ def fetch_news(request=None):
                 try:
                     pub_date_str = article.get('pubDate', None)
                     if pub_date_str:
-                        pub_date = datetime.strptime(pub_date_str, '%Y-%m-%d %H:%M:%S')
+                        pub_date = datetime.strptime(
+                            pub_date_str, '%Y-%m-%d %H:%M:%S')
                         pub_date = timezone.make_aware(pub_date)
-                    slug = slugify(article['title']) + '-' + str(uuid.uuid4())[:8]
+                    slug = slugify(article['title']) + \
+                        '-' + str(uuid.uuid4())[:8]
                     doc = nlp(article.get('content', ''))
                     sentences = [sent.text for sent in doc.sents]
-                    
+
                     def group_into_paragraphs(sentences, n):
                         paragraphs = []
                         for i in range(0, len(sentences), n):
                             paragraph = " ".join(sentences[i:i+n])
                             paragraphs.append(paragraph)
                         return paragraphs
-                    
+
                     paragraphs = group_into_paragraphs(sentences, 5)
                     formatted_content = "\n\n".join(paragraphs)
                     news_article, created = NewsArticle.objects.update_or_create(
@@ -68,19 +72,25 @@ def fetch_news(request=None):
                         }
                     )
                     if request:
-                        messages.success(request, f"Article saved with ID: {news_article.id}")
+                        messages.success(
+                            request, f"Article saved with ID: {news_article.id}")
                 except Exception as e:
-                    tb_str = traceback.format_exception(type(e), e, e.__traceback__)
+                    tb_str = traceback.format_exception(
+                        type(e), e, e.__traceback__)
                     tb_str = "".join(tb_str)
                     logger.error(f"Failed to save article: {e}\n{tb_str}")
         except KeyError:
-            logger.error(f"API response missing 'articles' key: {response.text}")
+            logger.error(
+                f"API response missing 'articles' key: {response.text}")
         except Exception as e:
             tb_str = traceback.format_exception(type(e), e, e.__traceback__)
             tb_str = "".join(tb_str)
-            logger.error(f"An error occurred while processing the API response: {e}\n{tb_str}")
+            logger.error(
+                f"An error occurred while processing the API response: {e}\n{tb_str}")
     else:
-        logger.error(f"API call failed with status code {response.status_code}: {response.text}")
+        logger.error(
+            f"API call failed with status code {response.status_code}: {response.text}")
+
 
 def make_api_call():
     api_key = os.getenv('NEWS_API_KEY')
@@ -88,17 +98,20 @@ def make_api_call():
     return requests.get(url)
 
 # Function to render news articles
+
+
 def news_article_list(request):
     articles = NewsArticle.objects.filter(Q(status=1))
     return render(request, 'index.html', {'news_article_list': articles})
 
 # Function to render individual news article details
+
+
 def news_article_detail(request, id):
     article = get_object_or_404(NewsArticle, id=id)
     return render(request, 'news_article_detail.html', {'article': article})
 
 
-    
 # Feedback
 def feedback_view(request):
     if request.method == 'POST':
@@ -111,6 +124,8 @@ def feedback_view(request):
     return render(request, 'feedback.html', {'form': form})
 
 # Signup view
+
+
 def account_signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -123,6 +138,8 @@ def account_signup(request):
     return render(request, 'account/signup.html', {'form': form})
 
 # Login view
+
+
 def user_login(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
@@ -135,6 +152,8 @@ def user_login(request):
     return render(request, 'account/login.html', {'form': form})
 
 # Logout view
+
+
 def user_logout(request):
     logout(request)
     return redirect('home')
