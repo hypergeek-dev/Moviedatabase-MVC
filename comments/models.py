8e7702df88d38e16e6cf5b8ed08c6ef71663920b
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-02-08 18:43:25 +0000

    Refactoring models for comments in accordance with PEP8

--- comments/models.py
+++ comments/models.py
@@ -2,11 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Comment(models.Model):
+    """
+    Model representing a comment made on a news article.
+    """
     news_article = models.ForeignKey('qfb_main.NewsArticle', related_name='comments', on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_comments', null=True)
     name = models.CharField(max_length=80)
     email = models.EmailField()
-    comment_content = models.TextField()
+    content = models.TextField()  # Renamed for simplicity
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=True)
 
@@ -14,4 +17,4 @@ class Comment(models.Model):
         ordering = ["created_on"]
 
     def __str__(self):
-        return f"Comment {self.comment_content} by {self.name}"
+        return f"Comment {self.content} by {self.name}"
