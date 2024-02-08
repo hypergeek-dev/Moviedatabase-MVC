92cc97ffa57000b488b09d92344865b86bbe97a7
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-02-08 01:35:30 +0000

    Changing paths in comment app to align with overall structure

--- comments/urls.py
+++ comments/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('comments/add_comment/<int:article_id>/', views.add_comment_to_article, name='add_comment_to_article'),
+    path('add_comment/<int:article_id>/', views.add_comment_to_article, name='add_comment_to_article'),
     path("edit_comment/<int:comment_id>/", views.edit_comment, name="edit_comment"),
     path("delete_comment/<int:comment_id>/", views.delete_comment, name="delete_comment"),
 ]
