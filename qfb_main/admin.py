7e7815057123ec196cb21a24f588b82f95bc3d6c
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-02-08 18:43:43 +0000

    refactoring admin config in accordance with PEP8

--- qfb_main/admin.py
+++ qfb_main/admin.py
@@ -3,7 +3,11 @@ from .models import NewsArticle
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(NewsArticle)
-class PostAdmin(SummernoteModelAdmin):
+class NewsArticleAdmin(SummernoteModelAdmin):
+    """
+    Admin view for editing NewsArticle models with enhanced features
+    provided by SummernoteModelAdmin for rich text editing.
+    """
     list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ['title', 'content']
     list_filter = ('status', 'created_on')
@@ -11,5 +15,14 @@ class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
 
     def has_change_permission(self, request, obj=None):
+        """
+        Ensure that only superusers can change news article entries.
+        
+        Args:
+            request: HttpRequest object.
+            obj: The object being changed; None if this is a 'change list' page.
+            
+        Returns:
+            bool: True if the user is a superuser, False otherwise.
+        """
         return request.user.is_superuser
-
