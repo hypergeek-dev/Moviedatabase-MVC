5bc6c8dbc79e514ec5dbc357fbffb5b8b0a47456
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-02-08 19:23:10 +0000

    Refactored urls.py in accordance with PEP8

--- comments/urls.py
+++ comments/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+# URL patterns for comment-related actions
 urlpatterns = [
     path('add_comment/<int:article_id>/', views.add_comment_to_article, name='add_comment_to_article'),
     path("edit_comment/<int:comment_id>/", views.edit_comment, name="edit_comment"),
