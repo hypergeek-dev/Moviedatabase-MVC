a61fec5424071a79b1119c7d5167ea9e58e6f697
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-02-08 18:43:02 +0000

    Refactoring comments form in accordance with PEP8

--- comments/forms.py
+++ comments/forms.py
@@ -2,8 +2,11 @@ from django import forms
 from .models import Comment
 
 class CommentForm(forms.ModelForm):
-    comment_content = forms.CharField(widget=forms.Textarea, required=True)
+    """
+    A form for creating and updating comments.
+    """
+    comment_content = forms.CharField(widget=forms.Textarea, required=True) 
 
     class Meta:
         model = Comment
-        fields = ('comment_content',)
+        fields = ('comment_content',)  
