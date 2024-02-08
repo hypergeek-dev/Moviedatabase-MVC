5581e62299051596dd1b0d503380331de13e6894
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-02-08 18:44:10 +0000

    Refactoring fetcher function in accordance with PEP8

--- qfb_main/management/commands/call_news.py
+++ qfb_main/management/commands/call_news.py
@@ -1,17 +1,25 @@
 from django.core.management.base import BaseCommand
 from qfb_main.views import fetch_news
-import logging  
+import logging
 
-# Initialize logging
+# It's recommended to configure logging in settings.py for larger projects
 logging.basicConfig(level=logging.DEBUG)
 
 class Command(BaseCommand):
+    """
+    A custom Django management command to fetch news and store it in the database.
+    """
+    
+    help = 'Fetches news from an external source and stores it in the database.'
 
     def handle(self, *args, **kwargs):
+        """
+        Executes the fetch_news function and handles success or failure.
+        """
         try:
             fetch_news()
-            self.stdout.write(self.style.SUCCESS('Successfully fetched news and stored it in the database'))
-            logging.debug("Successfully executed fetch_news")  
+            self.stdout.write(self.style.SUCCESS('Successfully fetched news and stored it in the database.'))
+            logging.debug("Successfully executed fetch_news")
         except Exception as e:
-            self.stdout.write(self.style.ERROR('Failed to fetch news and store it in the database'))
+            self.stdout.write(self.style.ERROR('Failed to fetch news and store it in the database.'))
             logging.error(f"Failed to execute fetch_news: {e}")
