1ba5c76032ad83dde517d0f6a113a23987098f6a
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-02-14 21:10:59 +0000

    Readding imports that got deleted by mistake

--- qfb_main/views.py
+++ qfb_main/views.py
@@ -11,6 +11,7 @@ from django.contrib.auth import login, logout
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.forms import UserCreationForm
 from django.utils import timezone
 from django.utils.text import slugify
 
