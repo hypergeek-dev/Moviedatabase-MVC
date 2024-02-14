e58ca011614cc8cd2b09ef0c9b5c074251351af4
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-02-14 00:48:18 +0000

    Moved views to own feedback app

--- feedback/views.py
+++ feedback/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def feedback_view(request):
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your feedback has been submitted successfully.')
+            return redirect('home')
+        else:
+            messages.error(request, 'Please correct the errors below.')
+    else:
+        form = FeedbackForm()
+    return render(request, 'feedback.html', {'form': form})
