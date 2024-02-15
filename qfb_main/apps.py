2a9714f50ae6364f9c8e859cd107e31eecdd6d30
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-02-15 22:29:56 +0000

    Changing human readable name of app to newsarticles

--- qfb_main/apps.py
+++ qfb_main/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class QfbMainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'qfb_main'
+    verbose_name = 'Newsarticles' 
