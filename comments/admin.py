b85187d3b5b64807b1fe2e8f30a869dc51c83403
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-02-15 16:32:25 +0000

    Adding admin accessibility in footer

--- comments/admin.py
+++ comments/admin.py
@@ -1,1 +1,13 @@
 from django.contrib import admin
+from .models import Comment
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_on')  
+    list_filter = ('approved', 'created_on')  
+    search_fields = ('name', 'email', 'comment_content') 
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+    approve_comments.short_description = "Mark selected comments as approved"
+
+admin.site.register(Comment, CommentAdmin)
--- feedback/admin.py
+++ feedback/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Feedback
 
-# Register your models here.
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_at')  
+    list_filter = ('created_at',)  
+    search_fields = ('name', 'email', 'message') 
+
+admin.site.register(Feedback, FeedbackAdmin)
